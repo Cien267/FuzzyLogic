4329172f3f1b765ea1ce8c0ab01b1ea47becb482
NGUYEN DUC KIEN <kien.nd@kiotviet.com>
2022-10-17 17:21:59 +0700

    add get label functions

--- src/main/java/ptit/tri_thuc/EvaluatingEconomicUsingFuzzyApplication.java
+++ src/main/java/ptit/tri_thuc/EvaluatingEconomicUsingFuzzyApplication.java
@@ -39,10 +39,10 @@ public class EvaluatingEconomicUsingFuzzyApplication {
                 {"SP"}, {"VL", "LO", "ME"}, {"MPC"}, {"HUR"}, {"1"}
             },
             {
-                {"SP"}, {"HI", "VH"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"}
+                {"SP"}, {"HI", "VH"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"} // triplicated
             },
             {
-                {"MP"}, {"VL"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"}
+                {"MP"}, {"VL"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"} // triplicated
             },
             {
                 {"MP"}, {"LO", "ME", "HI"}, {"LPC"}, UNEMPLOYMENT_RATE_VARIABLES, {"1"}
@@ -51,10 +51,10 @@ public class EvaluatingEconomicUsingFuzzyApplication {
                 {"MP"}, {"LO", "ME", "HI"}, {"MPC"}, {"HUR"}, {"1"}
             },
             {
-                {"MP"}, {"VH"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"}
+                {"MP"}, {"VH"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"} // triplicated
             },
             {
-                {"LP"}, {"VL", "LO"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"}
+                {"LP"}, {"VL", "LO"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"} // triplicated
             },
             {
                 {"LP"}, {"ME", "HI", "VH"}, {"LPC"}, UNEMPLOYMENT_RATE_VARIABLES, {"1"}
@@ -71,10 +71,10 @@ public class EvaluatingEconomicUsingFuzzyApplication {
                     {"SP"}, {"VL", "LO", "ME"}, {"HPC"}, {"HUR"}, {"0.5"}
             },
             {
-                    {"SP"}, {"HI", "VH"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"}
+                    {"SP"}, {"HI", "VH"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"} // triplicated
             },
             {
-                    {"MP"}, {"VL"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"}
+                    {"MP"}, {"VL"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"} // triplicated
             },
             {
                     {"MP"}, {"LO", "ME", "HI"}, {"MPC"}, {"LUR"}, {"1"}
@@ -86,10 +86,10 @@ public class EvaluatingEconomicUsingFuzzyApplication {
                     {"MP"}, {"LO", "ME", "HI"}, {"HPC"}, {"HUR"}, {"0.5"}
             },
             {
-                    {"MP"}, {"VH"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"}
+                    {"MP"}, {"VH"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"} // triplicated
             },
             {
-                    {"LP"}, {"VL", "LO"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"}
+                    {"LP"}, {"VL", "LO"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"} // triplicated
             },
             {
                     {"LP"}, {"ME", "HI", "VH"}, {"MPC"}, {"LUR"}, {"1"}
@@ -106,10 +106,10 @@ public class EvaluatingEconomicUsingFuzzyApplication {
                     {"SP"}, {"VL", "LO", "ME"}, {"HPC"}, {"LUR", "MUR"}, {"1"}
             },
             {
-                    {"SP"}, {"HI", "VH"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"}
+                    {"SP"}, {"HI", "VH"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"} // triplicated
             },
             {
-                    {"MP"}, {"VL"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"}
+                    {"MP"}, {"VL"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"} // triplicated
             },
             {
                     {"MP"}, {"LO", "ME", "HI"}, {"HPC"}, {"LUR"}, {"1"}
@@ -118,10 +118,10 @@ public class EvaluatingEconomicUsingFuzzyApplication {
                     {"MP"}, {"LO", "ME", "HI"}, {"HPC"}, {"MUR"}, {"0.5"}
             },
             {
-                    {"MP"}, {"VH"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"}
+                    {"MP"}, {"VH"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"} // triplicated
             },
             {
-                    {"LP"}, {"VL", "LO"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"}
+                    {"LP"}, {"VL", "LO"}, GDP_PER_CAPITA_VARIABLES, UNEMPLOYMENT_RATE_VARIABLES, {"0"} // triplicated
             },
             {
                     {"LP"}, {"ME", "HI", "VH"}, {"HPC"}, {"LUR"}, {"1"}
@@ -133,7 +133,13 @@ public class EvaluatingEconomicUsingFuzzyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EvaluatingEconomicUsingFuzzyApplication.class, args);
-        HashMap<String, String> result = EvaluatingEconomicUsingFuzzyApplication.evaluatingEconomicLevel("SP", "HI", "HPC", "HUR");
+        String populationLabel, GDPLabel, GDPPerCapitaLabel, unemploymentRateLabel;
+        populationLabel = EvaluatingEconomicUsingFuzzyApplication.getPopulationLabel(1);
+        GDPLabel = EvaluatingEconomicUsingFuzzyApplication.getGDPLabel(2);
+        GDPPerCapitaLabel = EvaluatingEconomicUsingFuzzyApplication.getGDPPerCapitaLabel(3);
+        unemploymentRateLabel = EvaluatingEconomicUsingFuzzyApplication.getUnemploymentRateLabel(4);
+
+        HashMap<String, String> result = EvaluatingEconomicUsingFuzzyApplication.evaluatingEconomicLevel(populationLabel, GDPLabel, GDPPerCapitaLabel, unemploymentRateLabel);
 
         System.out.println(result);
     }
@@ -166,6 +172,151 @@ public class EvaluatingEconomicUsingFuzzyApplication {
             return "MP";
         } else {
             return "LP";
+        }
+    }
+
+    public static String getGDPLabel(int x) {
+        int VLValue, LOValue, MEValue, HIValue, VHValue, result;
+
+        if (x <= 50000) {
+            VLValue = 1;
+        } else if (x <= 100000) {
+            VLValue = (1000000 - x)/50000;
+        } else {
+            VLValue = 0;
+        }
+
+        if (x <= 0) {
+            LOValue = 0;
+        } else if (x <= 100000) {
+            LOValue = x/100000;
+        } else if (x <= 200000) {
+            LOValue = (200000 - x)/100000;
+        } else {
+            LOValue = 0;
+        }
+
+        if (x <= 100000) {
+            MEValue = 0;
+        } else if (x <= 200000) {
+            MEValue = (x - 100000)/100000;
+        } else if (x <= 500000) {
+            MEValue = 1;
+        } else if (x <= 600000) {
+            MEValue = (600000 - x)/100000;
+        } else {
+            MEValue = 0;
+        }
+
+        if (x <= 500000) {
+            HIValue = 0;
+        } else if (x <= 600000) {
+            HIValue = (x - 500000)/100000;
+        } else if (x <= 900000) {
+            HIValue = 1;
+        } else if (x <= 1000000) {
+            HIValue = (1000000 - x)/100000;
+        } else {
+            HIValue = 0;
+        }
+
+        if (x <= 900000) {
+            VHValue = 0;
+        } else if (x <= 1000000) {
+            VHValue = (x - 900000)/100000;
+        } else {
+            VHValue = 1;
+        }
+
+        result = Math.max(VLValue,Math.max(LOValue, Math.max(MEValue, Math.max(HIValue, VHValue))));
+        if (result == VLValue) {
+            return "VL";
+        } else if (result == LOValue) {
+            return "LO";
+        } else if (result == MEValue) {
+            return "ME";
+        } else if (result == HIValue) {
+            return "HI";
+        } else {
+            return "VH";
+        }
+    }
+
+    public static String getGDPPerCapitaLabel(int x) {
+        int LPCValue, MPCValue, HPCValue, result;
+
+        if (x <= 0) {
+            LPCValue = 1;
+        } else if (x <= 30000) {
+            LPCValue = (30000 - x)/30000;
+        } else {
+            LPCValue = 0;
+        }
+
+        if (x <= 0) {
+            MPCValue = 0;
+        } else if (x <= 15000) {
+            MPCValue = x/15000;
+        } else if (x <= 30000) {
+            MPCValue = (30000 - x)/30000;
+        } else {
+            MPCValue = 0;
+        }
+
+        if (x <= 0) {
+            HPCValue = 0;
+        } else if (x <= 30000) {
+            HPCValue = x/30000;
+        } else {
+            HPCValue = 1;
+        }
+
+        result = Math.max(LPCValue,Math.max(MPCValue, HPCValue));
+        if (result == LPCValue) {
+            return "LPC";
+        } else if (result == MPCValue) {
+            return "MPC";
+        } else {
+            return "HPC";
+        }
+    }
+
+    public static String getUnemploymentRateLabel(int x) {
+        int LURValue, MURValue, HURValue, result;
+
+        if (x <= 0) {
+            LURValue = 1;
+        } else if (x <= 20) {
+            LURValue = (20 - x)/20;
+        } else {
+            LURValue = 0;
+        }
+
+        if (x <= 0) {
+            MURValue = 0;
+        } else if (x <= 10) {
+            MURValue = x/10;
+        } else if (x <= 20) {
+            MURValue = (20 - x)/10;
+        } else {
+            MURValue = 0;
+        }
+
+        if (x <= 0) {
+            HURValue = 0;
+        } else if (x <= 20) {
+            HURValue = x/20;
+        } else {
+            HURValue = 1;
+        }
+
+        result = Math.max(LURValue,Math.max(MURValue, HURValue));
+        if (result == LURValue) {
+            return "LUR";
+        } else if (result == MURValue) {
+            return "MUR";
+        } else {
+            return "HUR";
         }
     }
 
